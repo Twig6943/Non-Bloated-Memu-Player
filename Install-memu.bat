d3de700b404cf4f2660d124bbce10c5febd76fc5
Twig6943 <119701717+Twig6943@users.noreply.github.com>
2023-06-12 15:28:35 +0300

    Add files via upload

---
+++ Install-memu.bat
@@ -0,0 +1,6 @@
+color a
+cd C:\Users\%USERNAME%\Downloads\
+curl https://dl.memuplay.com/download/MEmu-Setup-9.0.1.0-ha55e8dab9.exe -o memu.exe
+C:\Users\%USERNAME%\Downloads\memu.exe
+echo First open memu, and then enable root acsess in settings. Secondly install neolauncher and then create a shortcut to your desktop (Do not delete that shortcut) thirdly open neolauncher and then uninstall the MEmu Launcher2. and then use that shortcut to open memu player.
+pause
---
+++ README.txt
@@ -0,0 +1,2 @@
+If you accidentally removed the shortcut just recreate it using the text down below.
+"C:\Program Files\Microvirt\MEmu\Memu.exe" MEmu applink com.saggitt.omega/com.saggitt.omega.OmegaLauncher
